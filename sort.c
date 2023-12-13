470bf6de16383fa15c303858ecbd48abbef7f7d1
Fabio Carvalho Lopes <facarval@paul-f4Br5s7.clusters.42paris.fr>
2023-12-13 18:47:01 +0100

    e

--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@
 /*   By: facarval <facarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 15:28:47 by facarval          #+#    #+#             */
-/*   Updated: 2023/12/13 15:16:45 by facarval         ###   ########.fr       */
+/*   Updated: 2023/12/13 18:46:38 by facarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,8 +79,8 @@ int	len_list(t_pile **liste)
 	return (i);
 }
 
-//trouve le moins couteux a push en fonction du chunks
-int	pos_nearest(t_pile **liste, int chunk)
+// trouve le moins couteux a push en fonction du chunk_sizes
+int	pos_nearest(t_pile **liste, int chunk_size)
 {
 	t_pile	*current;
 	int		res2;
@@ -97,7 +97,7 @@ int	pos_nearest(t_pile **liste, int chunk)
 	current = *liste;
 	while (res == 0)
 	{
-		if (chunk >= current->number)
+		if (chunk_size >= current->number)
 			res = i;
 		i++;
 		current = current->next;
@@ -109,14 +109,17 @@ int	pos_nearest(t_pile **liste, int chunk)
 	i = len_list(liste);
 	while (res2 == 0)
 	{
-		if (chunk >= current->number)
+		if (chunk_size >= current->number)
 			res2 = i;
 		i--;
 		current = current->previous;
 	}
 	res2 = len_list(liste) - i;
 	if (res > res2)
+	{
+		if (res2 )
 		return (i + 1);
+	}
 	return (res);
 }
 
@@ -128,81 +131,49 @@ void	sort(t_pile **stack_a, t_pile **stack_b)
 	int		len;
 	int		pos;
 	int		cost;
-	float	chunk;
-	int		i;
+	float	chunk_size;
+	int chunk_number;
 	int		max_pos;
+	float		total_len;
 
 	max_pos = 0;
-	i = 0;
 	compteur = 1;
 	min_pos = min_list(stack_a) - 1;
-	if (len_list(stack_a) == 100)
+	total_len = len_list(stack_a);
+	if (total_len > 400)
+		chunk_number = 12;
+	else
+		chunk_number = 6;
+	chunk_size = total_len / chunk_number;
+	while (compteur < total_len)
 	{
-		chunk = 10;
-		while (compteur < 100)
+		len = len_list(stack_a);
+		while (compteur <= chunk_size)
 		{
-			len = len_list(stack_a);
-			while (compteur <= chunk)
+			pos = pos_nearest(stack_a, chunk_size);
+			if (pos < len / 2)
 			{
-				pos = pos_nearest(stack_a, chunk);
-				if (pos < len / 2)
+				while (pos - 1)
 				{
-					while (pos - 1)
-					{
-						ra(stack_a, 1);
-						pos--;
-					}
-					pb(stack_a, stack_b);
+					ra(stack_a, 1);
+					pos--;
 				}
-				else
-				{
-					cost = len + 1 - pos;
-					while (cost)
-					{
-						rra(stack_a, 1);
-						cost--;
-					}
-					pb(stack_a, stack_b);
-				}
-				len--;
-				compteur++;
+				pb(stack_a, stack_b);
 			}
-			chunk += 10;
-		}
-	}
-	else if (len_list(stack_a) == 500)
-	{
-		chunk = 50;
-		while (compteur < 500)
-		{
-			len = len_list(stack_a);
-			while (compteur <= chunk)
+			else
 			{
-				pos = pos_nearest(stack_a, chunk);
-				if (pos < len / 2)
+				cost = len + 1 - pos;
+				while (cost)
 				{
-					while (pos - 1)
-					{
-						ra(stack_a, 1);
-						pos--;
-					}
-					pb(stack_a, stack_b);
+					rra(stack_a, 1);
+					cost--;
 				}
-				else
-				{
-					cost = len + 1 - pos;
-					while (cost)
-					{
-						rra(stack_a, 1);
-						cost--;
-					}
-					pb(stack_a, stack_b);
-				}
-				len--;
-				compteur++;
+				pb(stack_a, stack_b);
 			}
-			chunk += 50;
+			len--;
+			compteur++;
 		}
+		chunk_size += total_len / chunk_number;
 	}
 	while (1)
 	{
